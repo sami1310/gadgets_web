a69557d163b349ee183dd4e81fb610ae55e266d2
sami1310 <samia.jahin@northsouth.edu>
2022-10-20 11:57:17 +0600

    models_inti

--- gadgets/front_end/models.py
+++ gadgets/front_end/models.py
@@ -1,3 +1,29 @@
 from django.db import models
 
+
+CATEGORY_CHOICES = (
+    ('D', 'Desktop'),
+    ('L', 'Laptop'),
+    ('M', 'Mobile'),
+    ('C', 'Camera')
+)
+
+LABEL_CHOICES = (
+    ('N', 'New'),
+    ('S', 'SecondHand')
+)
+
+
 # Create your models here.
+class Item(models.Model):
+    title = models.CharField(max_length=80)
+    price = models.FloatField()
+    category = models.CharField(choices=CATEGORY_CHOICES, max_length=2)
+    label = models.CharField(choices=LABEL_CHOICES, max_length=2)
+    discount = models.FloatField()
+    description = models.TextField()
+    slug = models.SlugField()
+    image = models.ImageField()
+
+    def __str__(self):
+        return self.title
